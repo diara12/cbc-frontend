cdc764e334e10b1f8f993169393b32330a3e8181
diara12 <diarapahandi@gmail.com>
2025-04-26 20:18:21 +0530

    css positioning

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,15 @@ function App() {
   
 
   return (
-    <div className='w-full h-screen bg-red-100 flex flex-col justify-center items-center'>  
-      <div className='w-[100px] h-[100px] bg-blue-600'></div>
-      <div className='w-[100px] h-[100px] bg-yellow-600'></div>
-      <div className='w-[100px] h-[100px] bg-green-600'></div>
-      <div className='w-[100px] h-[100px] bg-purple-600'></div>
-      
+    <div className='w-full h-screen bg-red-100 flex flex-col justify-center items-center'> 
+      <div className =' relative w-[650px] h-[650px] bg-red-900 flex flex-col justify-center items-center'>
+        <div className ='w-[600px] h-[600px] bg-amber-100 flex flex-col justify-center items-center'>
+          <div className='w-[100px] h-[100px] bg-blue-600 absolute top-[0px] left-[0px]'></div>   {/* fixed will cause that div to move where are we want*/}
+          <div className='w-[100px] h-[100px] bg-yellow-600'></div>
+          <div className='w-[100px] h-[100px] bg-green-600'></div>
+          <div className='w-[100px] h-[100px] bg-purple-600'></div>
+        </div>
+      </div>
       
     </div>
   )
