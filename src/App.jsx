a8c923db0d0a5cee15eab3af090bae584ddad3e4
diara12 <diarapahandi@gmail.com>
2025-04-26 20:05:41 +0530

    flex-display

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,14 @@ function App() {
   
 
   return (
-    <>
-      <Header/>
-      <ProductCard/>
-    </>
+    <div className='w-full h-screen bg-red-100 flex flex-col justify-center items-center'>  
+      <div className='w-[100px] h-[100px] bg-blue-600'></div>
+      <div className='w-[100px] h-[100px] bg-yellow-600'></div>
+      <div className='w-[100px] h-[100px] bg-green-600'></div>
+      <div className='w-[100px] h-[100px] bg-purple-600'></div>
+      
+      
+    </div>
   )
 }
 
